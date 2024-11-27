56ec10e743c201bef43d39b1dea037daf0b6a316
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-11-27 17:35:15 +0900

    Create code_carte_mere.py

---
+++ code_carte_mere.py
@@ -0,0 +1,5 @@
+"""
+Cette carte va simplement garder en memoire la situation de chaque composant du systeme.
+Elle va utiliser ses digital pins pour indiquer un drapeau vert ou rouge pour l'action de chaque composant.
+Ceci peut et doit etre modifie par tout les membres du groupe en fonction de ses besoins en flag, attention, n'utiliser par plusieur fois le meme pin.
+"""
