53c0c633c19868e85e70b439979a4f89169088a2
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-11-29 08:44:11 +0900

    Update deplacement_bac_eau.py

--- Phase 1/Deplacement bac eau/deplacement_bac_eau.py
+++ Phase 1/Deplacement bac eau/deplacement_bac_eau.py
@@ -5,3 +5,31 @@ Dimensions : 50cm (longueur), 35cm (largeur), 45cm (hauteur)
 - fonction qui emmene d'un point A à un point B
 - fonction inverse qui emmene d'un point B à un point B
 """
+
+#Set-up
+#Importation des librairies
+import machine
+import time
+
+#Initialisation des pins
+signal_in_pin = 
+signal_out_pin = 
+
+#Creation des fonctions
+def aller_robot():
+
+
+def aller_papier():
+
+
+
+#Loop
+while True:
+  while signal_in_pin.value() == 0:
+    time.sleep(50)
+  aller_robot()
+  while signal_in_pin.value() == 0:
+    time.sleep(50)
+  aller_papier()
+  
+  
