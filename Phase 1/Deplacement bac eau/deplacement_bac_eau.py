46da9bd421c4b1839f81ee354c965897355622fa
Math-cot <mathilde.cottet@gmail.com>
2024-11-29 08:16:43 +0900

    Update deplacement_bac_eau.py

--- Phase 1/Deplacement bac eau/deplacement_bac_eau.py
+++ Phase 1/Deplacement bac eau/deplacement_bac_eau.py
@@ -2,4 +2,6 @@
 Mettre des commentaires cf robot_arm.py
 Objetcif : Déplacer un bac d'eau de 60kg sur une longueur d'1m
 Dimensions : 50cm (longueur), 35cm (largeur), 45cm (hauteur)
+- fonction qui emmene d'un point A à un point B
+- fonction inverse qui emmene d'un point B à un point B
 """
