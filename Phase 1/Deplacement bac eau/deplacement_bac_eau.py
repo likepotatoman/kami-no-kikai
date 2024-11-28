e88b0adcfc076a6b96577a554f2d0b745266d205
Math-cot <mathilde.cottet@gmail.com>
2024-11-29 08:14:11 +0900

    Update deplacement_bac_eau.py

--- Phase 1/Deplacement bac eau/deplacement_bac_eau.py
+++ Phase 1/Deplacement bac eau/deplacement_bac_eau.py
@@ -1,3 +1,5 @@
 """
-Mettre des commentaire cf robot_arm.py
+Mettre des commentaires cf robot_arm.py
+Objetcif : Déplacer un bac d'eau de 60kg sur une longueur d'1m
+Dimensions : 50cm (longueur), 35cm (largeur), 45cm (hauteur)
 """
