f470d4be4b1722464309a1160b534753b06b02f1
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-11-29 09:43:31 +0900

    Update robot_arm.py

--- Phase 1/Bras robotique/robot_arm.py
+++ Phase 1/Bras robotique/robot_arm.py
@@ -15,3 +15,99 @@ Dans ce code, nous allons creer :
 - une liste d'instructions qui va faire passer le sukata dans l'eau
 """
 
+#Set-up
+#importation des librairies
+import machine
+import time
+
+#Initialisation des pins
+signal_in_pin = 
+signal_out_pin =
+motor_1_pin = 
+motor_2_pin = 
+motor_3_pin = 
+servo_pin = 
+
+#Initialisation des variables des positions initiales
+alpha_actuel = 
+theta_actuel = 
+tau_actuel = 
+beta_actuel = 
+beta_prime_actuel = 
+epsilon_actuel = 
+phi_actuel = 
+lambda_actuel = 
+I_x_actuel = 
+I_y_actuel = 
+M_x_actuel = 
+M_y_actuel = 
+
+#Creation des fonctions
+def spin_motor_1(n_steps, direction):
+
+
+def spin_motor_2(n_steps, direction):
+
+
+def spin_motor_3(n_steps, direction):
+
+
+def move(M_x_but, M_y_but, phi_but):
+  alpha_but = 
+  theta_but = 
+  tau_but = 
+  beta_but = 
+  beta_prime_but = 
+  epsilon_but =  
+  I_x_but = 
+  I_y_but = 
+
+  delta_theta = 
+  delta_beta_prime = 
+  delta_phi = 
+
+  spin_motor_1()
+  spin_motor_2()
+  spin_motor_3()
+
+  alpha_actuel = alpha_but
+  theta_actuel = theta_but
+  tau_actuel = tau_but
+  beta_actuel = beta_but
+  beta_prime_actuel = beta_prime_but
+  epsilon_actuel = epsilon_but
+  phi_actuel = phi_but
+  lambda_actuel = lambda_but
+  I_x_actuel = I_x_but
+  I_y_actuel = I_y_but
+  M_x_actuel = M_x_but
+  M_y_actuel = M_y_but
+
+def fermer_pince():
+
+
+def ouvrir_pince():
+  
+
+def neutral():
+  move()
+
+def sukata_pass(n):
+  for i in range(n):
+    move()
+    move()
+    move()
+    move()
+
+def recup_sukata():
+  move()
+  fermer_pince()
+
+def drop_sukata():
+  move()
+  ouvrir_pince()
+
+
+#Loop
+while True:
+  
