c1bc2b91b2a2577fc495c4a8bf4e612748db2755
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-12-06 09:38:30 +0900

    Update code_carte_mere.py

--- code_carte_mere.py
+++ code_carte_mere.py
@@ -2,6 +2,29 @@
 Cette carte va simplement garder en memoire la situation de chaque composant du systeme.
 Elle va utiliser ses digital pins pour indiquer un drapeau vert ou rouge pour l'action de chaque composant.
 Ceci peut et doit etre modifie par tout les membres du groupe en fonction de ses besoins en flag, attention, n'utiliser par plusieur fois le meme pin.
+
+Liste des connections : 
+  CM --> Bras robotique : 
+    Position du sukata (numbre de colonnes + nombre de lignes)
+    sukata ouvert (1)
+    sukata sec (1)
+    START (1)
+  Bras robotique --> CM :
+    Fini (1)
+
+  CM --> Prep. papier : 
+    START (1)
+    Allumer shredder + mixer (1)
+  Prep. papier --> CM :
+    Papier (1)
+
+  CM --> Station Sechage : 
+    START (1)
+    Fini (1)
+  Station Sechage --> CM :
+    Position sukata (numbre de colonnes + nombre de lignes)
+    Sukata ouvert (1)
+    Sukata sec (1)
 """
 
 #Set-up
