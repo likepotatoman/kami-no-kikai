265031f0eb00114ff77e604dd82dfe5ef99c32b0
Kazushi Lambert <kazushi.lambert@gmail.com>
2024-12-06 13:23:11 +0900

    Update robot_arm.py

--- Phase 1/Bras robotique/robot_arm.py
+++ Phase 1/Bras robotique/robot_arm.py
@@ -30,6 +30,8 @@ motor_3_pin =
 servo_pin = 
 
 #Initialisation des variables des positions initiales
+position = 
+sukata_position = [0,0]
 alpha_actuel = 
 theta_actuel = 
 tau_actuel = 
@@ -44,14 +46,29 @@ M_x_actuel =
 M_y_actuel = 
 
 #Creation des fonctions
-def spin_motor_1(n_steps, direction):
+def spin_motor_1(n_steps, direction): #motor at O
 
 
-def spin_motor_2(n_steps, direction):
+def spin_motor_2(n_steps, direction): #motor at I
 
 
-def spin_motor_3(n_steps, direction):
+def spin_motor_3(n_steps, direction): #motor at M
 
+
+def spin_motor_4(n_steps, direction): #motor for rail
+
+
+def spin_motor_5(n_steps, direction): #motor on rotating platform
+
+
+def turn_on_vacuum():
+
+
+def turn_off_vacuum():
+
+
+def turn_on_shredder_mixer():
+  
 
 def move(M_x_but, M_y_but, phi_but):
   global alpha_actuel, theta_actuel, tau_actuel, beta_actuel, beta_prime_actuel, epsilon_actuel, phi_actuel, lambda_actuel, I_x_actuel, I_y_actuel, M_x_actuel, M_y_actuel
@@ -85,10 +102,10 @@ def move(M_x_but, M_y_but, phi_but):
   M_x_actuel = M_x_but
   M_y_actuel = M_y_but
 
-def fermer_pince():
+def close_claw():
 
 
-def ouvrir_pince():
+def open_claw():
   
 
 def neutral():
@@ -101,15 +118,95 @@ def sukata_pass(n):
     move()
     move()
 
+def lower_claw():
+  while #limit swtch off :
+    goal_y = M_y_actuel - #valeur arbitraire
+    move(M_x_actuel, goal_y, 0)
+  
+def raise_claw():
+  move(M_x_actuel, M_y_actuel - #valeur arbitraire, 0)
+
 def recup_sukata():
   move()
-  fermer_pince()
+  lower_claw()
+  close_claw()
+  raise_claw()
+  neutral()
 
 def drop_sukata():
   move()
-  ouvrir_pince()
+  open_claw()
+  neutral()
 
+def recup_mixer():
+  move()
+  lower_claw()
+  close_claw()
+  raise_claw()
+  neutral()
 
+def drop_mixer():
+  move()
+  open_claw()
+  neutral()
+
+def recup_vacuum():
+  move()
+  lower_claw()
+  close_claw()
+  raise_claw()
+  neutral()
+
+def drop_vacuum():
+  move()
+  open_claw()
+  neutral()
+
+def recup_paper():
+  move()
+  move()
+  turn_on_vacuum() 
+  move()
+  neutral()
+
+def drop_paper():
+  move()
+  turn_off_vacuum()
+  neutral()
+
+def negative_rail_move():
+  while #limit swtch on : 
+    spin_motor_4( , #neg)
+
+def positive_rail_move():
+  while #limit swtch on : 
+    spin_motor_4( , #pos)
+
+def rail_move(goal_position):
+  global position
+  while goal_position != position : 
+    if goal_position > position:
+      positive_rail_move()
+    else : 
+      negative_rail_move():
+
+def pass_cycle():
+  rail_move(#position du  vacuum)
+  recup_vacuum()
+  rail_move(#position du papier)
+  recup_paper()
+  rail_move(#position du shredder)
+  turn_on_shredder_mixer()
+  drop_paper()
+  rail_move(#position du vacuum)
+  drop_vacuum()
+  rail_move(#position du mixer)
+  recup_mixer()
+  
+  
+  
+
+  
 #Loop
 while True:
   
