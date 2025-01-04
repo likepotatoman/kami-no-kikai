db491f1deb446e4e87ecfef0f173b21404068818
Kazushi Lambert <kazushi.lambert@gmail.com>
2025-01-04 20:45:45 +0900

    Update and rename Phase 1/Deplacement bac eau/deplacement_bac_eau.py to Station de sechage/station_sechage.py

---
+++ Station de sechage/station_sechage.py
@@ -0,0 +1,19 @@
+"""
+Mettre des commentaires cf robot_arm.py
+Objetcif : Fournir a la carte mere l'information sur la position des sukatas et leur etat
+- fonction qui seche les sukatas 
+- fonction qui verifie l'etat des sukatas
+- fonction informe la carte mere sur quel sukata prendre
+"""
+
+#Set-up
+#Importation des librairies
+import machine
+import time
+
+#Initialisation des pins
+
+
+#Creation des fonctions
+
+  
